a9522581ef9198889154962ee6887ac039b86381
Julio Cesar S. Cunha <jcesarsc@gmail.com>
2024-10-22 12:00:35 -0300

    Insert/Update files and project from Ms Learn Path
    Insert/Update files and project from Ms Learn Path

---
+++ Data/PizzaService.cs
@@ -0,0 +1,19 @@
+namespace BlazingPizza.Data
+{
+    public class PizzaService
+    {
+        // public Task<Pizza[]> GetPizzasAsync()
+        // {
+        //     // Call your data access technology here
+        //     List<Pizza> specials = new();
+        //     specials.AddRange(new List<Pizza>
+        //     {
+        //         new Pizza { Name = "The Baconatorizor", Description = "It has EVERY kind of bacon" },
+        //         new Pizza { Name = "Buffalo chicken", Description = "Spicy chicken, hot sauce, and blue cheese, guaranteed to warm you up" },
+        //         new Pizza { Name = "Veggie Delight", Description = "It's like salad, but on a pizza" }
+        //     });
+
+        //     return Task.FromResult(specials);
+        // }
+    }
+}
